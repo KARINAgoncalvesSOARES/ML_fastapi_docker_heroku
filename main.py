e2ca893ca6b56a0cb91d1883815ab58b8909bcb1
KARINAgoncalvesSOARES <karinagonsoares@gmail.com>
2024-04-24 19:58:31 -0300

    API com FastAPI.

---
+++ main.py
@@ -0,0 +1,41 @@
+"""
+Execução do script
+==================
+
+$ uvicorn main:app --reload  (sem name == main)
+ou
+$ python main.py (Mantendo o name == main)
+
+"""
+from fastapi import FastAPI
+import uvicorn
+from pydantic import BaseModel # valida dados de entrada e de saída
+from app.modelo.model import predict_pipeline # Módulo específicado em minha aplicação (meu modelo)
+from app.modelo.model import __version__ as model_version
+
+
+# Criando instância
+app = FastAPI()
+
+# Valida od dados de entrada que a minha api espera receber
+class TextIn(BaseModel):
+    text: str
+
+# Valida os dados de saída que o modelo irá retornar
+class PredictionOut(BaseModel):
+    language: str
+    
+# Verificação de integridade
+@app.get("/")
+def home():
+    return {"health_check": "OK", "model_version": model_version}
+
+# Solicitação HTTP POST(enviar dados)
+@app.post("/predict", response_model=PredictionOut)
+def predict(payload: TextIn):
+    language = predict_pipeline(payload.text)
+    return {"language": language}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
